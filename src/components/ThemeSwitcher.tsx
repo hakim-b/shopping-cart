c7f62ace6ab2e86fd2338006ef6363f343f7d4f1
hakim-b <hakimbouabdellah02@gmail.com>
2022-10-15 12:33:27 -0400

    added toggle switch

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { themeChange } from "theme-change";
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 
 export function ThemeSwitcher() {
   useEffect(() => {
@@ -8,24 +9,14 @@ export function ThemeSwitcher() {
 
   return (
     <>
-      <div className="btn-group">
-        <input
-          type="radio"
-          name="options"
-          data-title="☀️"
-          className="btn btn-ghost text-base"
-          data-set-theme="emerald"
-          data-act-class="ACTIVECLASS"
-        />
-        <input
-          type="radio"
-          name="options"
-          data-title="🌙"
-          className="btn btn-ghost text-base"
-          data-set-theme="dracula"
-          data-act-class="ACTIVECLASS"
-        />
-      </div>
+      <SunIcon className="h-10 w-10" />
+      <input
+        type="checkbox"
+        className="toggle toggle-lg"
+        data-toggle-theme="dracula,emerald"
+        data-act-class="ACTIVECLASS"
+      />
+      <MoonIcon className="h-10 w-10" />
     </>
   );
 }
